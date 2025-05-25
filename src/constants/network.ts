53761f39c0f598d2e6f8c5fc3e23ef37c4b209f7
Itzli Molina <itzli2000@msn.com>
2025-05-24 18:24:09 -0600

    feat(Home): add new sections for hero, featured posts, about preview, and connect
    - Introduced new components for HeroSection, FeaturedPostsSection, AboutPreviewSection, and ConnectSection to enhance the homepage layout.
    - Updated the index pages to utilize these new components, improving content organization and user engagement.
    - Implemented bilingual support for all new sections, ensuring accessibility for both English and Spanish users.
    - Enhanced localization files to include new content for the homepage, enriching the user experience.

---
+++ src/constants/network.ts
@@ -0,0 +1,5 @@
+export const NETWORK = {
+  GITHUB: "https://github.com/Itzli2000",
+  LINKEDIN: "https://www.linkedin.com/in/ariel-itzli-molina-s%C3%A1nchez-0b4a09129/",
+  EMAIL: "mailto:ariel.molina.dev@gmail.com",
+};
